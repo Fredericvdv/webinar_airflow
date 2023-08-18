2f42f0d255a1f181f960ee785308e25b2fa1e7b6
Fredericvdv <frederic1vanderveken@gmail.com>
2023-08-18 12:26:43 +0000

    add additional sensor arguments

--- workspace/mount/dags/2_sensor_downstream.py
+++ workspace/mount/dags/2_sensor_downstream.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -10,18 +10,27 @@ with DAG(
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
+    
     start_task = BashOperator(
         task_id="start_task",
         bash_command="echo 'Start task'",
     )
+
     sensor_A = ExternalTaskSensor(
         task_id="sensor_A",
         external_dag_id="2_sensor_upstream_A",
-        external_task_id="end_task",
+        external_task_id="end_task_A_marker",
+        timeout=60,                 # in secondes
+        allowed_states=["success"],
     )
+
     sensor_B = ExternalTaskSensor(
         task_id="sensor_B",
         external_dag_id="2_sensor_upstream_B",
         external_task_id="end_task_B_marker",
+        timeout=30,                 # in secondes
+        allowed_states=["success"],
+        execution_delta= timedelta(minutes=1)
     )
+    
     start_task >> sensor_A >> sensor_B
--- workspace/mount/dags/2_sensor_upstream_A.py
+++ workspace/mount/dags/2_sensor_upstream_A.py
@@ -6,7 +6,7 @@ from airflow.sensors.external_task import ExternalTaskMarker
 
 with DAG(
     dag_id="2_sensor_upstream_A",
-    schedule='* * * * *',
+    schedule='*/2 * * * *',
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
