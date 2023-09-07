b4067e457253f1f3f7f428c6225be93db749131c
Frederic Vanderveken <frederic.vanderveken@dataminded.be>
2023-09-07 12:54:21 +0200

    change dag name

--- workspace/mount/dags/2_sensor.py
+++ workspace/mount/dags/2_sensor.py
@@ -67,7 +67,7 @@ with DAG(
 
     sensor_A = ExternalTaskSensor(
         task_id="sensor_A",
-        external_dag_id="2_sensor_upstream_A",
+        external_dag_id="2_sensor_upstream_dag_A",
         external_task_id="end_task_A_marker",
         timeout=60,                 # Time (in seconds) before the task times out and fails
         allowed_states=["success"],
@@ -75,7 +75,7 @@ with DAG(
 
     sensor_B = ExternalTaskSensor(
         task_id="sensor_B",
-        external_dag_id="2_sensor_upstream_B",
+        external_dag_id="2_sensor_upstream_dag_B",
         external_task_id="end_task_B_marker",
         timeout=30,                 
         allowed_states=["success"],
