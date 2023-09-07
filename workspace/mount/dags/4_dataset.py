9bfc1818d1bb8fe73522cf4a5e86a94fe9168800
Frederic Vanderveken <frederic.vanderveken@dataminded.be>
2023-09-07 12:12:47 +0200

    add more comments

--- workspace/mount/dags/4_dataset.py
+++ workspace/mount/dags/4_dataset.py
@@ -7,45 +7,50 @@ from airflow.datasets import Dataset
 dataset1 = Dataset('s3://folder1/dataset_1.txt')
 dataset2 = Dataset('s3://folder1/dataset_2.txt')
 
-# upstream A
+# ---------------------- UPSTREAM DAG A ---------------------- #
 with DAG(
     dag_id="4_dataset_upstream_A",
-    schedule='* * * * *',
+    schedule='* * * * *',      # Runs every minute
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
+    
     start_task = BashOperator(
         task_id="start_task",
         bash_command="echo 'Modify dataset_1.txt'",
         outlets=[dataset1],
     )
 
-# upstream B
+# ---------------------- UPSTREAM DAG B ---------------------- #
 with DAG(
     dag_id="4_dataset_upstream_B",
-    schedule='*/3 * * * *',
+    schedule='*/2 * * * *',   # Runs every 2 minutes
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
+    
     start_task = BashOperator(
         task_id="start_task",
         bash_command="echo 'Modify dataset_2.txt'",
         outlets=[dataset2],
     )
 
-# downstream
+# ---------------------- DOWNSTREAM DAG ---------------------- #
 with DAG(
     dag_id="4_dataset_downstream",
     schedule=[dataset1, dataset2],
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
+    
     start_task = BashOperator(
         task_id="start_task",
         bash_command="echo 'Start task'",
     )
+
     end_task = BashOperator(
         task_id="end_task",
         bash_command="echo 'End task'",
     )
+
     start_task >> end_task
